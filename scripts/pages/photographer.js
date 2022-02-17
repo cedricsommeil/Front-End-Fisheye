1ac6123d24b5eda14bcb648a8900d651293109d1
cedricsommeil <cedricsulon18@gmail.com>
2022-02-17 17:07:58 +0100

    correction du html et css

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,30 +5,31 @@
 let params = (new URL(document.location)).searchParams;
 let id = params.get('id');
 // console.log(id);
-const photographers = fetch("data/photographers.json")
+const photographers = fetch("../../data/photographers.json")
   .then(response => response.json())
   .then(data => {
 // On recupere les informations du photographes en se servant de son ID
-console.log(data.photographers);
 
         const photographerInfo = data.photographers.filter((photographer)=>{
                 return photographer.id === Number(id);
 
         });
-        const mediaInfo = data.medias.filter((media)=>{
+
+        const mediaInfo = data.media.filter((media)=>{
                 return media.photographerId === Number(id);
- });
+        });
  
-console.log(photographerInfo);
-        createPhotographerInfos(photographerInfo)
-        createMediaInfo(mediaInfo)
+        createPhotographerInfos(photographerInfo);
+
+        createMediaInfo(mediaInfo);
 
   }).catch(console.error);
 
   function createMediaInfo(data){
 
-  const mediaInfo = /* faire une boucle car il ya plusieurs photo et on veut 
+   /* faire une boucle car il ya plusieurs photo et on veut 
   que sa s'affiche dans un ordre particulier for sur les elements en rajoutant un ul et li */ 
+ 
 
 }
 
@@ -39,13 +40,13 @@ console.log(photographerInfo);
       
       <div class="info_photographer">
         <h2>${data[0].name}</h2>
-        <p class="photograph-villCity">${data[0].city}, ${data[0].country}</p>
-        <p class="para_grey">${data[0].tagline}</p>
         <button class="contact_button">Contactez-moi</button>
         <div class="photo_photographer">
             <img src="assets/images/Sample Photos/Photographers ID Photos/${data[0].portrait}" alt="${data[0].portrait}">
         </div>
       </div>
+      <p class="photograph-villCity">${data[0].city}, ${data[0].country}</p>
+      <p class="para_grey">${data[0].tagline}</p>
       
       `;
 
